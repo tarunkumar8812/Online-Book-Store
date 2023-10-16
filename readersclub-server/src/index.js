ca5613e1c715fcec912f9da4da7dace12c3d87e5
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-10-16 21:36:37 +0530

    first commit

---
+++ readersclub-server/src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const cors = require('cors')
+require("dotenv").config()
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL || "mongodb+srv://TarunKumar123:xLcX9W1SI9646ftM@cluster1.tpwtwiv.mongodb.net/ReadersClub", {
+    useNewUrlParser: true
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+
+app.use(cors())
+
+// app.use('/', route);
+
+app.listen((process.env.PORT || 3000), function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
+
+
+// app.use("/*", function (req, res) {
+//     return res.status(400).send({ status: false, message: "invalid request params (path not found)" })
+// });
