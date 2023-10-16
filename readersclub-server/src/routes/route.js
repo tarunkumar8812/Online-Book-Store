a5b82749a5d3acc5498f71054ef73acfa2d4f7e7
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-10-16 22:04:53 +0530

    unnesesary files cleared

--- readersclub-server/src/routes/route.js
+++ readersclub-server/src/routes/route.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 
 router.get("/", (req, res) => {
-    return res.status(200).json({message:"frist route"})
+    return res.status(200).json({ message: "frist route" })
 })
 
 router.get("/hello", (req, res) => {
-    return res.status(200).json({message:"hello route"})
+    return res.status(200).json({ message: "hello route" })
 })
 module.exports = router
